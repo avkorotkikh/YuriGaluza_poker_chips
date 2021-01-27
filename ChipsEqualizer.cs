6e71fb0aaf694cb644c42ef839c11c757e53577d
Юрий Галуза <yourchello@mail.ru>
2021-01-28 04:07:52 +0600

    Добавьте файлы проекта.

---
+++ ChipsEqualizer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace poker_chips
+{
+    class ChipsEqualizer
+    {
+        public int ChipsEqualize(List<int> chips) {
+            int count = 0;
+            
+            for (int j = 0; j < chips.Count; j++)
+            {
+                for (int i = 1; i < chips.Count; i++) 
+                {
+                    int diff = chips[i] - chips[i - 1];
+                    if (diff != 0 && Math.Abs(diff) > 1)
+                    {
+                        chips[i] = chips[i] - diff / 2;
+                        chips[i - 1] = chips[i - 1] + diff / 2;
+                        count += Math.Abs(diff / 2);
+                    }
+                    else if (i == chips.Count - 1) {
+                        int diffEnd = chips[0] - chips[chips.Count - 1];
+                        chips[0] = chips[0] - diffEnd / 2;
+                        chips[i] = chips[i] + diffEnd / 2;
+                        count += Math.Abs(diffEnd / 2);
+                    }
+                }
+            }
+            Console.WriteLine("chip moves = " + count);
+            return count;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace poker_chips
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            ChipsEqualizer equalizer = new ChipsEqualizer();
+
+            equalizer.ChipsEqualize(new List<int>{ 1, 5, 9, 10, 5 });
+            equalizer.ChipsEqualize(new List<int> { 1, 2, 3 });
+            equalizer.ChipsEqualize(new List<int> { 0, 1, 1, 1, 1, 1, 1, 1, 1, 2 });
+            
+        }
+    }
+}
+
