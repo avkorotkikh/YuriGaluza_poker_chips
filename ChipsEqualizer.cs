57d48f0a277f38ba2ac77762fb32a0a06e4b3211
YuriGaluza <74955101+YuriGaluza@users.noreply.github.com>
2021-01-29 07:37:19 +0600

    Add files via upload

--- ChipsEqualizer.cs
+++ ChipsEqualizer.cs
@@ -1,35 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace poker_chips
 {
     class ChipsEqualizer
     {
-        public int ChipsEqualize(List<int> chips) {
+        public int ChipsEqualize(List<int> chips, int expected) {
             int count = 0;
-            
-            for (int j = 0; j < chips.Count; j++)
+            int diff;
+            int chipsLength = chips.Count;
+            int average = (int)Average(chips);
+
+            for (int j = 0; j < chipsLength; j++)
             {
-                for (int i = 1; i < chips.Count; i++) 
+                for (int i = 0; i < chipsLength; i++)
                 {
-                    int diff = chips[i] - chips[i - 1];
-                    if (diff != 0 && Math.Abs(diff) > 1)
+                    if (i < chipsLength - 1)
                     {
-                        chips[i] = chips[i] - diff / 2;
-                        chips[i - 1] = chips[i - 1] + diff / 2;
-                        count += Math.Abs(diff / 2);
+                        diff = chips[i + 1] - chips[i];
+                        if (Math.Abs(diff) > 1)
+                        {
+                            chips[i] = chips[i] + diff / 2;
+                            chips[i + 1] = chips[i + 1] - diff / 2;
+                            count += Math.Abs(diff / 2);
+                        }
                     }
-                    else if (i == chips.Count - 1) {
-                        int diffEnd = chips[0] - chips[chips.Count - 1];
-                        chips[0] = chips[0] - diffEnd / 2;
-                        chips[i] = chips[i] + diffEnd / 2;
-                        count += Math.Abs(diffEnd / 2);
+                    else if (i == chipsLength - 1)
+                    {
+                        diff = chips[0] - chips[chipsLength - 1];
+                        if (Math.Abs(diff) > 1)
+                        {
+                            chips[chipsLength - 1] = chips[chipsLength - 1] + diff / 2;
+                            chips[0] = chips[0] - diff / 2;
+                            count += Math.Abs(diff / 2);
+                        }
                     }
                 }
             }
-            Console.WriteLine("chip moves = " + count);
+            Console.WriteLine($"chip moves = {count} expected {expected}");
             return count;
         }
+
+        public int Sum(List<int> list) {
+            int sum = 0;
+            foreach (int elem in list) {
+                sum += elem;
+            }
+            return sum;
+        }
+
+        public double Average(List<int> list) {
+            double result = 1.0 * Sum(list) / list.Count;
+            return result;
+}
     }
 }
